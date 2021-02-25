65a595b1b5a0664d5d9cc3b3cd2678e0e6e0a06f
JM Duval <jmarieduval@hotmail.fr>
2021-02-25 10:15:01 +0100

    mise en page

--- all_book_in_category.py
+++ all_book_in_category.py
@@ -1,39 +1,35 @@
 # -*-coding: utf-8 -*
-# Script permettant de scraper les informations d'une catégorie d'ouvrage
+""" This program is attached to the 'main.py' program. The target is find all the links of books
+in category.
+"""
 
-# Import libraries
 import requests
 import csv
 from bs4 import BeautifulSoup
 from math import *
 
-#--link_all_book_in_category______________--
-def get_all_book_in_category (category_link):
-        #print (category_link)
-        print (category_link)
+
+# --link_all_book_in_category------------------
+def get_all_book_in_category(category_link):
         response = requests.get(category_link)
         soup = BeautifulSoup(response.text, "html.parser")
         # --Pagination---------------
-        page = soup.find('form',{'method':'get'}).text
-        result= page[3:5]
-        affich= page[29:31]
+        page = soup.find('form', {'method': 'get'}).text
+        result = page[3:5]
+        affich = page[29:31]
         book_links = []
-        if int(result) > 20 :
+        if int(result) > 20:
                 nb_page = ceil(int(result) / int(affich))
-                #print (nb_page)
-                #print ('Nombre de page: ',nb_page)
-                for x in range (1, nb_page+1):
-                        url = category_link[:-6]
-                        #print (x)
-                        url_comp = url + str(x) + str('.html')
-                        #print (url_comp)
+                for x in range(1, nb_page + 1):
+                        url = category_link[:-10]
+                        url_comp = url + 'page-' + str(x) + str('.html')
                         response1 = requests.get(url_comp)
                         soup1 = BeautifulSoup(response1.text, "html.parser")
                         h3 = soup1.findAll('h3')
                         for ahref in h3:
-                                url_book = ahref.find('a').get('href').replace('../../..','http://books.toscrape.com/catalogue')
+                                url_book = ahref.find('a').get('href') \
+                                        .replace('../../..', 'http://books.toscrape.com/catalogue')
                                 book_links.append(url_book)
-
                         x += 1
 
                 return book_links
@@ -42,22 +38,8 @@ def get_all_book_in_category (category_link):
                 soup1 = BeautifulSoup(response2.text, "html.parser")
                 h3 = soup1.findAll('h3')
                 for ahref in h3:
-                        url_book = ahref.find('a').get('href').replace('../../..','http://books.toscrape.com/catalogue')
+                        url_book = ahref.find('a').get('href') \
+                                .replace('../../..', 'http://books.toscrape.com/catalogue')
                         book_links.append(url_book)
 
                 return book_links
-
-# Script test
-#r = get_all_book_in_category(url_cat)
-#print (r)
-#url_cat = 'http://books.toscrape.com/catalogue/category/books/mystery_3/page-1.html'
-#response = requests.get(url_cat)
-
-# --Create a BeautifulSoup object----------------------
-#soup = BeautifulSoup(response.text, "html.parser")
-
-
-
-
-
-
