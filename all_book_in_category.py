2e97e9811ca60690c005f8a09f716061712d79c3
JM Duval <jmarieduval@hotmail.fr>
2021-02-25 15:35:45 +0100

    mise en page

--- all_book_in_category.py
+++ all_book_in_category.py
@@ -4,42 +4,42 @@ in category.
 """
 
 import requests
-import csv
+
 from bs4 import BeautifulSoup
-from math import *
+from math import ceil
 
 
-# --link_all_book_in_category------------------
 def get_all_book_in_category(category_link):
-        response = requests.get(category_link)
-        soup = BeautifulSoup(response.text, "html.parser")
-        # --Pagination---------------
-        page = soup.find('form', {'method': 'get'}).text
-        result = page[3:5]
-        affich = page[29:31]
-        book_links = []
-        if int(result) > 20:
-                nb_page = ceil(int(result) / int(affich))
-                for x in range(1, nb_page + 1):
-                        url = category_link[:-10]
-                        url_comp = url + 'page-' + str(x) + str('.html')
-                        response1 = requests.get(url_comp)
-                        soup1 = BeautifulSoup(response1.text, "html.parser")
-                        h3 = soup1.findAll('h3')
-                        for ahref in h3:
-                                url_book = ahref.find('a').get('href') \
-                                        .replace('../../..', 'http://books.toscrape.com/catalogue')
-                                book_links.append(url_book)
-                        x += 1
+    """link all book in category"""
+    response = requests.get(category_link)
+    soup = BeautifulSoup(response.text, "html.parser")
+    # --Pagination---------------
+    page = soup.find('form', {'method': 'get'}).text
+    result = page[3:5]
+    affich = page[29:31]
+    book_links = []
+    if int(result) > 20:
+        nb_page = ceil(int(result) / int(affich))
+        for page in range(1, nb_page + 1):
+            url = category_link[:-10]
+            url_comp = url + 'page-' + str(page) + str('.html')
+            response1 = requests.get(url_comp)
+            soup1 = BeautifulSoup(response1.text, "html.parser")
+            h3 = soup1.findAll('h3')
+            for ahref in h3:
+                url_book = ahref.find('a').get('href') \
+                    .replace('../../..', 'http://books.toscrape.com/catalogue')
+                book_links.append(url_book)
+            page += 1
 
-                return book_links
-        else:
-                response2 = requests.get(category_link)
-                soup1 = BeautifulSoup(response2.text, "html.parser")
-                h3 = soup1.findAll('h3')
-                for ahref in h3:
-                        url_book = ahref.find('a').get('href') \
-                                .replace('../../..', 'http://books.toscrape.com/catalogue')
-                        book_links.append(url_book)
+        return book_links
+    else:
+        response2 = requests.get(category_link)
+        soup1 = BeautifulSoup(response2.text, "html.parser")
+        ss_title = soup1.findAll('h3')
+        for ahref in ss_title:
+            url_book = ahref.find('a').get('href') \
+                .replace('../../..', 'http://books.toscrape.com/catalogue')
+            book_links.append(url_book)
 
-                return book_links
+        return book_links
