392189f77af8176e9c3dc9d1dfb7089965dced99
JM Duval <jmarieduval@hotmail.fr>
2021-02-25 15:36:06 +0100

    mise en page

--- all_category_link.py
+++ all_category_link.py
@@ -5,18 +5,19 @@ categories.
 
 
 import requests
-import csv
+
 from bs4 import BeautifulSoup
 
-# --All_category_links---------------------------------
-def get_all_category_link(url_bts):
-        response = requests.get(url_bts)
-        soup = BeautifulSoup(response.text, "html.parser")
-        uls=soup.findAll ('a')[3:-41]
-        category_dict = []
-        for a_tag in uls:
-                add_url = 'http://books.toscrape.com/'
-                cat_links = add_url+a_tag.attrs.get("href")
-                category_dict.append(cat_links)
 
-        return category_dict
+def get_all_category_link(url_bts):
+    """All category links"""
+    response = requests.get(url_bts)
+    soup = BeautifulSoup(response.text, "html.parser")
+    uls=soup.findAll ('a')[3:-41]
+    category_dict = []
+    for a_tag in uls:
+        add_url = 'http://books.toscrape.com/'
+        cat_links = add_url+a_tag.attrs.get("href")
+        category_dict.append(cat_links)
+
+    return category_dict
