12c36f2e3e25665c5c6fb34867cbe8af36558fc0
JM Duval <jmarieduval@hotmail.fr>
2021-02-25 10:14:18 +0100

    correction bug CSV

--- main.py
+++ main.py
@@ -1,69 +1,76 @@
 # -*-coding: utf-8 -*
-# main Script de book_to_Scrapt
+""" This program is a beta version of a script to automate a price monitoring system on a website.
+The target is to retrieve information from each book and write it to a CSV file. And more, this
+script download the picture of the plate book for each book.
+"""
 
-# --import_libraries-----------------------------------
-import requests
-import csv
-from bs4 import BeautifulSoup
-from book_infos import get_book_infos
-from all_book_in_category import get_all_book_in_category
-from all_category_link import get_all_category_link
-from pathlib import Path
-import pathlib
 import os
+import requests
+from bs4 import BeautifulSoup
+import csv
+import pathlib
+from pathlib import Path
+from book_infos import get_book_infos
+from all_category_link import get_all_category_link
+from all_book_in_category import get_all_book_in_category
 
 # -----------------------------------------------------
 url_bts = 'http://books.toscrape.com/'
 response = requests.get(url_bts)
-# --Create_a_BeautifulSoup_object----------------------
 soup = BeautifulSoup(response.text, "html.parser")
-
-
 # --Data_Path----------------------
 data = Path('data')
-if not os.path.exists(data):  # if data-(Path) don't exist, create data-(Path)
+if not os.path.exists(data):
         os.mkdir(data)
 
 # --Category_Path------------------
 uls = soup.findAll('a')[3:-41]
-#print (uls)
 for a_tag in uls:
-        category_folder = a_tag.attrs.get("href")[25:-13].replace("_", "").replace ("\n",",").replace('-','') #find all category
+        category_folder = a_tag.attrs.get("href")[25:-13].replace("_", "").replace("\n", ",") \
+                .replace('-', '')
         # -- CSV_Path -------------------------------
         dir_cat = os.path.join(data, category_folder)
-        if not os.path.exists(dir_cat):  # if dir_cat don't exist, create dir_cat
+        if not os.path.exists(dir_cat):
                 os.mkdir(dir_cat)
         # -- Image_Path -----------------------------
         img_path = os.path.join(dir_cat, 'image')
-        if not os.path.exists(img_path):  # if dir_cat don't exist, create dir_cat
+        if not os.path.exists(img_path):
                 os.mkdir(img_path)
 
 
 # --Create_CSV --------------------------------------
 def create_csv(book):
+        category = book['category']
+        if not os.path.isfile(os.path.join(data, category, category + '.csv')):
+                with open(os.path.join(data, category, category + '.csv'), 'a', newline='',
+                          encoding="utf-8-sig") as file:
+                        csv_writer = csv.DictWriter(file, fieldnames=book.keys(), delimiter=",")
+                        csv_writer.writeheader()
+                        csv_writer.writerow(book)
+        else:
+                with open(os.path.join(data, category, category + '.csv'), 'a', newline='',
+                          encoding="utf-8-sig") as file:
+                        csv_writer = csv.writer(file, delimiter=',', quotechar=' ',
+                                                quoting=csv.QUOTE_MINIMAL)
+                        csv_writer.writerow(book.values())
 
-        category = book['category']  # information given by book_infos
-        with open (os.stat(nom de fichier) os.path.join(data, category , category+ '.csv'), 'a', newline='', encoding="utf-8-sig") as file:
-                ecrire = csv.writer (file, delimiter= ',', quotechar=' ', quoting=csv.QUOTE_MINIMAL)
-                ecrire.writerow(book.values())
 
 # --Download_image---------------------
 def record_image (book):
-        title = book['title'][:8].replace('"', "")  # information given by book_infos
-        category = book['category']  # information given by book_infos
-        url_img = book['url image']  # information given by book_infos
-        url_image = requests.get(url_img)  # requests for catch imgae
-        with open(os.path.join(data, category , 'image' , title + '.jpeg'), 'wb') as f:
-                f.write(url_image.content)
-
+        title = book['title'][:8].replace('"', "").replace(':','_').replace('/','')
+        category = book['category']
+        url_img = book['url image']
+        url_image = requests.get(url_img)
+        with open(os.path.join(data, category , 'image' , title + '.jpeg'), 'wb') as file:
+                file.write(url_image.content)
 
 
 # --Main_Script--------------------------------------
-#if __name__ == '__main__':
-category_links = get_all_category_link(url_bts) # tous les liens des catégories
+# if __name__ == '__main__':
+category_links = get_all_category_link(url_bts)
 for category_link in category_links:
-        book_links = get_all_book_in_category(category_link) # tous les livres par catégorie
+        book_links = get_all_book_in_category(category_link)
         for book_link in book_links:
-                book = get_book_infos(book_link)  # information given by book_infos
+                book = get_book_infos(book_link)
                 create_csv(book)
                 record_image(book)
